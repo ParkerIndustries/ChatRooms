2776e142feb0749f5c95641e0c9ad78424dcecb2
ParkerIndustries <mickael.k.swaminathan@gmail.com>
2020-10-07 01:41:35 +0200

    work in progress

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import querystring from 'query-string';
-import io from 'socket.io-client';
+import io, { connect } from 'socket.io-client';
+
+let socket;
 
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
     useEffect(() => {
-        const data = querystring.parse(location.search);
+        const {name, room} = querystring.parse(location.search);
+
+        socket = io(ENDPOINT);
+
         setName(name);
         setRoom(room);
-    })
+
+        socket.emit('join', { name, room }, () => {
+        });
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
+    }, [ENDPOINT, location.search])
     return(
         <h1>Chat</h1>
     );
